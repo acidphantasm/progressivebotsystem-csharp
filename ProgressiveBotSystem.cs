272666b2cc1152636f851a3b0e47f97d58f3e1c9
acidphantasm <acidphantasm@acidphantasm.com>
2025-10-26 22:13:39 -0500

    Update license, I thought I already did this?

--- ProgressiveBotSystem.cs
+++ ProgressiveBotSystem.cs
@@ -18,12 +18,12 @@ public record ModMetadata : AbstractModMetadata, IModWebMetadata
     public override string Author { get; init; } = "acidphantasm";
     public override List<string>? Contributors { get; init; }
     public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
-    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.1");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.2");
     public override List<string>? Incompatibilities { get; init; } = ["li.barlog.andern"];
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public override string? Url { get; init; }
     public override bool? IsBundleMod { get; init; }
-    public override string? License { get; init; } = "MIT";
+    public override string? License { get; init; } = "CC BY-NC-ND 4.0";
 }
 
 [Injectable(TypePriority = OnLoadOrder.PreSptModLoader)]
