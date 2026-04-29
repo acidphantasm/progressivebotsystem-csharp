902f62e308d442f9f2db605cf0ad04dadf2e47e0
acidphantasm <acidphantasm@nobody.com>
2026-04-28 23:34:45 -0500

    Pascal Casing and fixing the UpdateView tracking

--- Web/Core/Utils.cs
+++ Web/Core/Utils.cs
@@ -60,171 +60,59 @@ namespace _progressiveBotSystem.Web.Core
             }
         }
 
-        public static void UpdateView(bool holder, bool originalConfigValue, [CallerMemberName] string caller = "")
+        private static void UpdatePendingState(bool changed, string caller)
         {
-            switch (MainLayout.pendingChanges.Contains(caller))
+            if (changed)
             {
-                case true:
-                    if (holder != originalConfigValue) return;
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-                case false:
-                    if (holder != originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Add(caller);
-                    }
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
+                MainLayout.pendingChanges.Add(caller);
+            }
+            else
+            {
+                MainLayout.pendingChanges.Remove(caller);
             }
 
             MainLayout.TriggerUIRefresh();
         }
-        public static void UpdateView(int holder, int originalConfigValue, [CallerMemberName] string caller = "")
-        {
-            switch (MainLayout.pendingChanges.Contains(caller))
-            {
-                case true:
-                    if (holder != originalConfigValue) return;
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-                case false:
-                    if (holder != originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Add(caller);
-                    }
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-            }
 
-            MainLayout.TriggerUIRefresh();
-        }
-        
-        public static void UpdateView(double holder, double originalConfigValue, [CallerMemberName] string caller = "")
+        public static void UpdateView(bool holder, bool originalConfigValue, string caller)
         {
-            switch (MainLayout.pendingChanges.Contains(caller))
-            {
-                case true:
-                    if (holder != originalConfigValue) return;
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-                case false:
-                    if (holder != originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Add(caller);
-                    }
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-            }
+            UpdatePendingState(holder != originalConfigValue, caller);
+        }
 
-            MainLayout.TriggerUIRefresh();
-        }
-        public static void UpdateView(List<string> holder, List<string> originalConfigValue, [CallerMemberName] string caller = "")
+        public static void UpdateView(int holder, int originalConfigValue, string caller)
         {
-            switch (MainLayout.pendingChanges.Contains(caller))
-            {
-                case true:
-                    if (!holder.SequenceEqual(originalConfigValue)) return;
-                    if (holder.SequenceEqual(originalConfigValue))
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-                case false:
-                    if (!holder.SequenceEqual(originalConfigValue))
-                    {
-                        MainLayout.pendingChanges.Add(caller);
-                    }
-                    break;
-            }
+            UpdatePendingState(holder != originalConfigValue, caller);
+        }
 
-            MainLayout.TriggerUIRefresh();
-        }
-        public static void UpdateView(List<int> holder, List<int> originalConfigValue, [CallerMemberName] string caller = "")
+        public static void UpdateView(double holder, double originalConfigValue, string caller)
         {
-            switch (MainLayout.pendingChanges.Contains(caller))
-            {
-                case true:
-                    if (!holder.SequenceEqual(originalConfigValue)) return;
-                    if (holder.SequenceEqual(originalConfigValue))
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-                case false:
-                    if (!holder.SequenceEqual(originalConfigValue))
-                    {
-                        MainLayout.pendingChanges.Add(caller);
-                    }
-                    break;
-            }
+            UpdatePendingState(Math.Abs(holder - originalConfigValue) > 0.0001d, caller);
+        }
 
-            MainLayout.TriggerUIRefresh();
-        }
-        public static void UpdateView([CallerMemberName] string caller = "")
+        public static void UpdateView(string holder, string originalConfigValue, string caller)
         {
-            switch (MainLayout.pendingChanges.Contains(caller))
-            {
-                case true:
-                    MainLayout.pendingChanges.Remove(caller);
-                    break;
-                case false:
-                    MainLayout.pendingChanges.Add(caller);
-                    break;
-            }
+            UpdatePendingState(holder != originalConfigValue, caller);
+        }
 
-            MainLayout.TriggerUIRefresh();
-        }
-        public static void UpdateView(bool isWeightChange, [CallerMemberName] string caller = "")
+        public static void UpdateView(List<string> holder, List<string> originalConfigValue, string caller)
         {
-            if (isWeightChange)
-            {
-                if (!MainLayout.pendingChanges.Contains(caller))
-                {
-                    MainLayout.pendingChanges.Add(caller);
-                }
-            }
+            UpdatePendingState(!holder.SequenceEqual(originalConfigValue), caller);
+        }
 
-            MainLayout.TriggerUIRefresh();
-        }
-        public static void UpdateView(string holder, string originalConfigValue, [CallerMemberName] string caller = "")
+        public static void UpdateView(List<int> holder, List<int> originalConfigValue, string caller)
         {
-            switch (MainLayout.pendingChanges.Contains(caller))
+            UpdatePendingState(!holder.SequenceEqual(originalConfigValue), caller);
+        }
+
+        public static void UpdateView(string caller)
+        {
+            if (MainLayout.pendingChanges.Contains(caller))
             {
-                case true:
-                    if (holder != originalConfigValue) return;
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
-                case false:
-                    if (holder != originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Add(caller);
-                    }
-                    if (holder == originalConfigValue)
-                    {
-                        MainLayout.pendingChanges.Remove(caller);
-                    }
-                    break;
+                MainLayout.pendingChanges.Remove(caller);
+            }
+            else
+            {
+                MainLayout.pendingChanges.Add(caller);
             }
 
             MainLayout.TriggerUIRefresh();
