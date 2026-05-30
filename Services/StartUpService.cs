bb279426da5db8c197c77219c95a7eea8486d0a6
acidphantasm <acidphantasm@nobody.com>
2026-05-30 02:02:50 -0500

    Split mod metadata

--- Services/StartUpService.cs
+++ Services/StartUpService.cs
@@ -10,22 +10,7 @@ using SPTarkov.Server.Core.Models.Spt.Mod;
 using SPTarkov.Server.Core.Utils;
 using Path = System.IO.Path;
 
-namespace _progressiveBotSystem;
-
-public record ModMetadata : AbstractModMetadata, IModWebMetadata
-{
-    public override string ModGuid { get; init; } = "com.acidphantasm.progressivebotsystem";
-    public override string Name { get; init; } = "Acid's Progressive Bot System";
-    public override string Author { get; init; } = "acidphantasm";
-    public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("2.2.0");
-    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.9");
-    public override List<string>? Incompatibilities { get; init; } = ["li.barlog.andern"];
-    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-    public override string? Url { get; init; }
-    public override bool? IsBundleMod { get; init; }
-    public override string? License { get; init; } = "CC BY-NC-ND 4.0";
-}
+namespace ProgressiveBotSystem.Services;
 
 [Injectable(TypePriority = OnLoadOrder.PreSptModLoader)]
 public class StartUpService(
